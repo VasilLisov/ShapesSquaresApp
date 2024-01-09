71bbeada346d76b422c7841c3b930206b2e9f5e7
Artsiom Pazdniakou <artsiom.pazdniakou.dev@gmail.com>
2024-01-09 21:18:01 +0300

    Common functionality created

---
+++ IShapesAreas.cs
@@ -0,0 +1,10 @@
+﻿namespace ShapesSquaresAreas;
+
+public interface IShapesAreas
+{
+    float GetArea(float radius);
+
+    float GetArea(float sideA, float sideB, float sideC);
+
+    bool CheckIfRightAngled(float sideA, float sideB, float sideC);
+}
---
+++ ShapesAreas.cs
@@ -0,0 +1,63 @@
+﻿namespace ShapesSquaresAreas;
+
+internal class ShapesAreas : IShapesAreas
+{
+    public bool CheckIfRightAngled(float sideA, float sideB, float sideC)
+    {
+        if (!CheckIfPositiveValue(sideA, sideB, sideC))
+        {
+            return false;
+        }
+
+        float max = Math.Max(sideA, sideB);
+
+        if (sideC > max)
+        {
+            max = sideC;
+        }
+
+        bool isRightAngled = sideA * sideA == sideB * sideB + sideC * sideC
+            || sideB * sideB == sideC * sideC + sideA * sideA
+            || sideC * sideC == sideA * sideA + sideB * sideB;
+
+        return isRightAngled;
+    }
+
+    public float GetArea(float radius)
+    {
+        if (!CheckIfPositiveValue(radius))
+        {
+            return 0.0f;
+        }
+
+        float roundArea = (float)(Math.PI * Math.Pow(radius, 2));
+
+        return roundArea;
+    }
+
+    public float GetArea(float sideA, float sideB, float sideC)
+    {
+        if (!CheckIfPositiveValue(sideA, sideB, sideC))
+        {
+            return 0.0f;
+        }
+
+        float semiPerimeter = (sideA + sideB + sideC) / 2;
+        float triangleArea = (float)Math.Sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+
+        return triangleArea;
+    }
+
+    bool CheckIfPositiveValue(params float[] values)
+    {
+        foreach (float item in values)
+        {
+            if (item < 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
